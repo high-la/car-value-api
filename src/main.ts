fe2171553229e8a2dcf3432da9e0dd73b3cf4791
Haile <hailecst@gmail.com>
2026-05-12 19:42:20 +0300

    session setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+const cookieSession = require('cookie-session')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({
+    keys: ['abc5e57e']
+  }))
   app.useGlobalPipes(
     new ValidationPipe({
       // a security property 
